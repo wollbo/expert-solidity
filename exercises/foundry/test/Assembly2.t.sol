ae3426561e7b3a15bd60757fa9d46cfee32dd1f0
wollbo <hildingw@gmail.com>
2023-02-25 21:10:33 +0100

    some work on hw6

--- exercises/foundry/test/Assembly2.t.sol
+++ exercises/foundry/test/Assembly2.t.sol
@@ -2,6 +2,19 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
+import "../src/Passed.sol";
+
 
 contract AssemblyTest is Test {
-    
+    Passed public passed;
+
+    function setUp() public {
+        passed = new Passed();
+    }
+
+    function testReturnCall() public {
+        uint call_val = 10;
+        passed.passed{value: call_val}();
+        
+    }
+}
